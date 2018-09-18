b22acdb075c33aa37a6181f645fc1ac1e94dd497
Shrijeet Karade <skarade@tavisca.com>
2018-09-18 12:43:38 +0530

    learning demo

---
+++ WcfServiceLibrary1/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Client.DemoServiceReference;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClient demoClient = new DemoClient("BasicHttpBinding_IDemo");
+            EmpData empData = new EmpData()
+            {
+                EmpId = "21",
+                EmpName = "shri"
+            };
+            Console.WriteLine(demoClient.GetEmpData(empData));
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ WcfServiceLibrary1/WcfServiceLibrary1/DemoService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    [DataContract]
+    public class EmpData
+    {
+        [DataMember]
+        public string EmpName; //data members
+        [DataMember]
+        public string EmpId;
+       
+    }
+    [ServiceContract]
+    public interface IDemo
+    {
+        [OperationContract]
+        string GetEmpData(EmpData empData);
+    }
+    public class Demo : IDemo
+    {
+        public string GetEmpData(EmpData empData)
+        {
+            return string.Format("Employee name {0} and emplyoee ID {1}",empData.EmpName,empData.EmpId);
+        }
+    }
+}
